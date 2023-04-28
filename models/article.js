906e0f27d21438c10de2df99fd491330b6a1e128
Jack Coots <jack.coots@newtoneurope.com>
2023-04-28 11:53:10 +0100

    Make Article Model

---
+++ models/article.js
@@ -0,0 +1,19 @@
+//require necessary NPM Packages
+const mongoose = require("mongoose");
+
+// Define Article Schema
+const articleSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: String,
+    author: { type: String, required: true },
+    published: { type: Boolean, default: true },
+    publishedOn: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+//Compile our model based on the article schema
+const Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
